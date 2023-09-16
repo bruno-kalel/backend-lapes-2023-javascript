2baf74fe43703747d4bf466a78815f61d5c4bad9
Bruno Kalel <104171372+bruno-kalel@users.noreply.github.com>
2023-09-16 16:20:20 -0300

    arrumando rotas, criando pasta separada pra rotas

--- api/index.js
+++ api/index.js
@@ -1,28 +1,17 @@
 // subir servidor local com express
 const express = require('express')
-// importar biblioteca que vai gerir os formatos dos dados
-const bodyParser = require('body-parser')
+
+// chamar a pasta de rotas, para esse código aqui acessar lá
+const routes = require('./routes')
 
 // para iniciar o express
 const app = express()
 
-// métodos do express
-// avisar o express do método json do bodyParser
-app.use(bodyParser.json())
-
 // porta 3000 será utilizada no servidor local
 const port = 3000
 
-// rota de teste, primeiro teste
-// método get do express funciona de forma parecida ao método get do http
-// recebe dois parâmetros,
-// parâmetro 1 nome da rota que será testada
-// parâmetro 2 função callback que recebe requisição e passa resposta
-// uma dessas informações vai ser a situação da requisição
-// depois se envia uma informação de volta
-app.get('/teste', (req, res) => res
-    .status(200)
-    .send({mensagem: 'boas-vindas'}))
+// importando métodos de routes, mandando o express lá para routes
+routes(app)
 
 // colocar o express para ficar ouvindo o servidor, se rola algum erro etc
 // método listen receber o número da porta
---
+++ api/routes/index.js
@@ -0,0 +1,19 @@
+// importar biblioteca que vai gerir os formatos dos dados
+const bodyParser = require('body-parser')
+
+module.exports = app =>
+{
+    // rota de teste, primeiro teste
+    // método get do express funciona de forma parecida ao método get do http
+    // recebe dois parâmetros,
+    // parâmetro 1 nome da rota que será testada
+    // parâmetro 2 função callback que recebe requisição e passa resposta
+    // uma dessas informações vai ser a situação da requisição
+    // depois se envia uma informação de volta
+    app.get('/teste', (req, res) => res
+        .status(200)
+        .send({mensagem: 'boas-vindas'}))
+
+    app.use(bodyParser.json())
+    app.get('/', (req, res) => res.send('aqui'))
+}
